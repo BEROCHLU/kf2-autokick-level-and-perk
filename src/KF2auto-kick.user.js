7d38dcd76c99f2e3e05ed082ca2ba5aa6e1e32b6
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2019-08-23 23:28:46 +0900

    fix memory leak issue free setInterval ID

--- src/KF2auto-kick.user.js
+++ src/KF2auto-kick.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KF2auto-kick
 // @namespace    monkey
-// @version      0.3
+// @version      0.4
 // @description  auto kick Level and Perk
 // @author       BEROCHlU
 // @match        http://*/ServerAdmin/*
@@ -10,57 +10,85 @@
 // @noframes
 // ==/UserScript==
 
+let g_time_id;
+
 (function() {
+    'use strict';
 
-    var MIN_LV;
-    var MAX_LV;
-    var arrKickperk = new Array(10);
+    let arrKickperk = new Array(10);
+    let timer_count = 0;
 
-    $( document ).ready(function() {
+    const kickTime = () => {
+        fetch('/ServerAdmin/current/players')
+            .then(response => response.text())
+            .then(data => {
 
-		var arrKickperkInit = ["Berserker","anonymous","anonymous","anonymous","Demolitionist","Firebug","anonymous","anonymous","anonymous","anonymous"];
+                if (data.indexOf("There are no players") != -1) {
+                    return;
+                }
 
-		localStorage.getItem("storageMin") || (localStorage.setItem("storageMin", "0"), console.log("localStorage MinLv initialized") );
-		localStorage.getItem("storageMax") || (localStorage.setItem("storageMax", "25"), console.log("localStorage MaxLv initialized") );
-		localStorage.getItem("storageKickperk") || (localStorage.setItem("storageKickperk", JSON.stringify(arrKickperkInit)), console.log("localStorage Kickperk initialized") );
+                const MIN_LV = parseInt(localStorage.getItem("storageMin"));
+                const MAX_LV = parseInt(localStorage.getItem("storageMax"));
 
-        setInterval(kickTime, 16000);
+                arrKickperk = JSON.parse(localStorage.getItem("storageKickperk")); //console.log(MIN_LV,MAX_LV,arrKickperk);
 
-    });
+                const players = JSON.parse(data.replace("}, ] }", "} ] }"));
 
-    function kickTime() {
+                for (let gamer of players.player) {
+                    if (gamer.perkName != "" &&
+                        (parseInt(gamer.perkLevel) < MIN_LV ||
+                            parseInt(gamer.perkLevel) > MAX_LV)) {
 
-        $.get( "/ServerAdmin/current/players", function( data ) {
+                        if (gamer.isSpectator == "No") {
+                            $.post("/ServerAdmin/current/players", {
+                                    playerkey: gamer.key,
+                                    action: "kick"
+                                }) &&
+                                $.post("/ServerAdmin/current/chat+frame", {
+                                    ajax: "1",
+                                    message: `auto-kick: ${gamer.perkName}-${gamer.perkLevel}`,
+                                    teamsay: "-1"
+                                })
+                            console.log(players);
+                        }
+                    } else if (arrKickperk.includes(gamer.perkName)) {
 
-            if (data.indexOf("There are no players") != -1){
-                return;
-            }
-          
-            MIN_LV = parseInt(localStorage.getItem("storageMin") );
-			MAX_LV = parseInt(localStorage.getItem("storageMax") );
-			arrKickperk = JSON.parse(localStorage.getItem("storageKickperk") );//console.log(MIN_LV,MAX_LV,arrKickperk);
-
-            var players = JSON.parse(data.replace("}, ] }","} ] }"));
-
-            for (var i=0; i<players.player.length; i++){
-                if (players.player[i].perkName != ""
-                    && (parseInt(players.player[i].perkLevel) < MIN_LV
-                    || parseInt(players.player[i].perkLevel) > MAX_LV) ){
-
-                    if(players.player[i].isSpectator == "No"){
-                        $.post("/ServerAdmin/current/players", { playerkey: players.player[i].key, action: "kick" })
-                        && $.post("/ServerAdmin/current/chat+frame", { ajax: "1", message: "auto-kick " + players.player[i].perkName + "-" + players.player[i].perkLevel, teamsay: "-1"})
-                        console.log(players);
-                    }
-                }else if(arrKickperk.includes(players.player[i].perkName) ){
-
-                    if(players.player[i].isSpectator == "No"){
-                        $.post("/ServerAdmin/current/players", { playerkey: players.player[i].key, action: "kick" })
-                        && $.post("/ServerAdmin/current/chat+frame", { ajax: "1", message: "auto-kick " + players.player[i].perkName + "-" + players.player[i].perkLevel, teamsay: "-1"})
-                        console.log(players);console.log('no demo fire bsk');
+                        if (gamer.isSpectator == "No") {
+                            $.post("/ServerAdmin/current/players", {
+                                    playerkey: gamer.key,
+                                    action: "kick"
+                                }) &&
+                                $.post("/ServerAdmin/current/chat+frame", {
+                                    ajax: "1",
+                                    message: `auto-kick: ${gamer.perkName}-${gamer.perkLevel}`,
+                                    teamsay: "-1"
+                                })
+                            console.log(players);
+                            console.log('no demo fire bsk');
+                        }
                     }
                 }
-            }
-        });
+                timer_count += 1;
+            })
+            .catch(e => {
+                console.log(e);
+            });
+        // test code
+        if (timer_count > 9000) {// 1H:225 20H:4500
+            clearInterval(g_time_id);
+            g_time_id = setInterval(kickTime, 16000);
+            timer_count = 0;
+        }
     }
+
+    $(document).ready(function() {
+
+        var arrKickperkInit = ["anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous"];
+
+        localStorage.getItem("storageMin") || (localStorage.setItem("storageMin", "0"), console.log("localStorage MinLv initialized"));
+        localStorage.getItem("storageMax") || (localStorage.setItem("storageMax", "25"), console.log("localStorage MaxLv initialized"));
+        localStorage.getItem("storageKickperk") || (localStorage.setItem("storageKickperk", JSON.stringify(arrKickperkInit)), console.log("localStorage Kickperk initialized"));
+
+        g_time_id = setInterval(kickTime, 16000);
+    });
 })();
