ee8c26576e483bb5e8a2be2eba62f2ed77e616c6
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2019-08-25 21:38:13 +0900

    厳密比較

--- KF2auto-kick.user.js
+++ KF2auto-kick.user.js
@@ -15,19 +15,19 @@ let g_time_id;
 (() => {
     'use strict';
 
-    let arrKickperk = new Array(10);
+    let arrKickperk = Array(10);
     let timer_count = 0;
 
     const asyncPostAll = async (gamer) => {
         const promises = [
-            $.post("/ServerAdmin/current/players", {
+            $.post('/ServerAdmin/current/players', {
                 playerkey: gamer.key,
-                action: "kick"
+                action: 'kick'
             }),
-            $.post("/ServerAdmin/current/chat+frame", {
-                ajax: "1",
+            $.post('/ServerAdmin/current/chat+frame', {
+                ajax: '1',
                 message: `auto-kick: ${gamer.perkName}-${gamer.perkLevel}`,
-                teamsay: "-1"
+                teamsay: '-1'
             })
         ];
 
@@ -41,50 +41,49 @@ let g_time_id;
             .then(response => response.text())
             .then(data => {
 
-                if (data.indexOf('There are no players') != -1) {
+                if (data.indexOf('There are no players') !== -1) {
                     return;
                 }
 
                 const MIN_LV = parseInt(localStorage.getItem("storageMin"));
                 const MAX_LV = parseInt(localStorage.getItem("storageMax"));
-
                 arrKickperk = JSON.parse(localStorage.getItem("storageKickperk")); //console.log(MIN_LV,MAX_LV,arrKickperk);
 
                 const players = JSON.parse(data.replace("}, ] }", "} ] }"));
 
                 for (let gamer of players.player) {
-                    if (gamer.perkName != "" &&
-                        (parseInt(gamer.perkLevel) < MIN_LV ||
-                            parseInt(gamer.perkLevel) > MAX_LV)) {
-
-                        if (gamer.isSpectator == 'No') {
-                            asyncPostAll(gamer);
-                        }
-                    } else if (arrKickperk.includes(gamer.perkName)) {
-
-                        if (gamer.isSpectator == 'No') {
-                            asyncPostAll(gamer);
-                            console.log('no demo fire bsk');
+                    if (gamer.perkName === '') {
+                        // do nothing
+                    } else {
+                        if (parseInt(gamer.perkLevel) < MIN_LV || parseInt(gamer.perkLevel) > MAX_LV) {
+                            if (gamer.isSpectator === 'No') {
+                                asyncPostAll(gamer);
+                            }
+                        } else if (arrKickperk.includes(gamer.perkName)) {
+                            if (gamer.isSpectator === 'No') {
+                                asyncPostAll(gamer);
+                                console.log('no demo fire bsk');
+                            }
                         }
                     }
                 }
                 timer_count += 1;
-                console.log(timer_count)
+                //console.log(timer_count)
             })
             .catch(e => {
                 console.log(e);
             });
-        // fix memory leak issue
-        if (timer_count > 9000) { // 1H:225 20H:4500
+        // improve memory leak issue
+        if (timer_count > 60) { // 1H:225 20H:4500
             clearInterval(g_time_id);
             g_time_id = setInterval(kickTime, 16000);
             timer_count = 0;
         }
     }
 
-    //main
-    {
-        var arrKickperkInit = ["anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous"];
+    { //main
+        //let arrKickperkInit = ["anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous", "anonymous"];
+        const arrKickperkInit = Array(10).fill('anonymous');
 
         localStorage.getItem("storageMin") || (localStorage.setItem("storageMin", "0"), console.log("localStorage MinLv initialized"));
         localStorage.getItem("storageMax") || (localStorage.setItem("storageMax", "25"), console.log("localStorage MaxLv initialized"));
